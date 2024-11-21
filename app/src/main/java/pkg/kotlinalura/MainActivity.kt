d75b0b8586b39d87b770fd9e93ad7b2605168b6c
Marcos Aurelio <marcos.carvalho@casafreitas.com.br>
2024-11-21 13:09:34 -0300

    Implementado na MainActivity os valores apresentados ao usuário

--- app/src/main/java/pkg/kotlinalura/MainActivity.kt
+++ app/src/main/java/pkg/kotlinalura/MainActivity.kt
@@ -9,8 +9,19 @@ import android.widget.Toast
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nome = findViewById<TextView>(R.id.nome)
+        nome.text = "Cesta de frutas"
+
+        val descricao = findViewById<TextView>(R.id.descricao)
+        descricao.text = "Laranja, manga e maçã"
+
+        val valor = findViewById<TextView>(R.id.valor)
+        valor.text = "19.99"
+
     }
 
 }
